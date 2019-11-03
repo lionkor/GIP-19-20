d28d8aa3c876d774fe8e26550dca67a6da46fad0
Lion Kortlepel <development@kortlepel.com>
2019-11-03 14:53:31 +0100

    Fix issues with the calculation of step>=60

--- 04.03.cpp
+++ 04.03.cpp
@@ -14,33 +14,35 @@ int main()
 
     do
     {
-    std::cout << "Bitte geben Sie die Minuten der Startuhrzeit ein: ";
-    std::cin >> start_time_minutes;
+        std::cout << "Bitte geben Sie die Minuten der Startuhrzeit ein: ";
+        std::cin >> start_time_minutes;
     } while (start_time_minutes < 0 || start_time_minutes > 59);
 
     std::cout << "Der erste Bus fährt also um " << start_time_hours << ":"
               << start_time_minutes << "Uhr ." << std::endl;
-    
+
     do
     {
-    std::cout << "Bitte geben Sie die Taktzeit in Minuten ein: ";
-    std::cin >> step_minutes;
+        std::cout << "Bitte geben Sie die Taktzeit in Minuten ein: ";
+        std::cin >> step_minutes;
     } while (step_minutes < 0 || step_minutes > 180);
 
     int hours = start_time_hours;
     int minutes = start_time_minutes;
-    
-    while (!(minutes + step_minutes > 60 && hours >= 23))
+
+    std::cout << hours << ":" << minutes << " ";
+
+    do
     {
         minutes += step_minutes;
         if (minutes >= 60)
         {
-            hours++;
-            minutes = minutes - 60;
+            hours += minutes / 60;
+            minutes = minutes % 60;
             std::cout << std::endl;
         }
         std::cout << hours << ":" << minutes << " ";
-    }
-    
+    } while (!(hours + (minutes + step_minutes) / 60 >= 24));
+
     std::cout << std::endl;
 }
