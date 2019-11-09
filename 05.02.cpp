48c1ffc3af506ffc372ebe77619addacbaadebf1
Lion Kortlepel <development@kortlepel.com>
2019-11-09 13:15:06 +0100

    Completed 05.02

--- 05.02.cpp
+++ 05.02.cpp
@@ -2,4 +2,29 @@
 
 int main()
 {
+    int array[5] { 0 };
+    for (unsigned i = 0; i < 5; ++i)
+    {
+        do
+        {
+            std::cout << "Bitte geben Sie die " << i + 1 << ". Zahl ein: ? ";
+            std::cin >> array[i];
+        } while (array[i] < 1 || array[i] > 9);
+    }
+
+    for (int row = 9; row > 0; --row)
+    {
+        std::cout << row;
+        for (unsigned i = 0; i < 5; ++i)
+        {
+            if (array[i] == row)
+                std::cout << '*';
+            else if (array[i] > row)
+                std::cout << '+';
+            else
+                std::cout << '.';
+        }
+        std::cout << std::endl;
+    }
+    std::cout << " 12345" << std::endl;
 }
