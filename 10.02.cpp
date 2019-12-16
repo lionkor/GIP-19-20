d03b74fd5dd78e8aadd50fa7f2644d8d90324192
Lion Kortlepel <development@kortlepel.com>
2019-12-16 13:07:24 +0100

    Complete 10.02

--- 10.02.cpp
+++ 10.02.cpp
@@ -10,8 +10,27 @@ int main()
     {
         return 1;
     }
-    // Ihr Code ab hier ...
 
+    std::cout << "Bitte geben Sie den Text ein: ";
+    std::string input;
+    std::getline(std::cin, input);
+    std::string token;
+    std::cout << "Bitte geben Sie die zu suchende Zeichenkette ein: ";
+    std::getline(std::cin, token);
+
+    int pos = zeichenkette_suchen(input.c_str(), token.c_str());
+    if (pos == -1)
+    {
+        std::cout << "Die Zeichenkette '" << token << "'  ist NICHT in dem Text '"
+                  << input << "' enthalten." << std::endl;
+    }
+    else
+    {
+        std::cout << "Die Zeichenkette '" << token << "' ist in dem Text '" << input
+                  << "' enthalten." << std::endl
+                  << "Sie started ab Zeichen " << pos << " (bei Zaehlung ab 0)."
+                  << std::endl;
+    }
 
     return 0;
 }
