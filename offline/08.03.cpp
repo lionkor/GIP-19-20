c2b66eddb277a3c6d252e929100060771e155e9e
Lion Kortlepel <development@kortlepel.com>
2020-01-11 13:26:11 +0100

    Solve 08.03 (proper)
    the last "solve 08.03" commit was wrong, i don't know how that happened!

---
+++ offline/08.03.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+char* my_strconcat(const char* ptr1, const char* ptr2);
+
+int main(int, char**) {
+    std::cout << "Bitte ersten Text eingeben (ggfs. mit Leerzeichen): ? ";
+    std::string s1;
+    std::getline(std::cin, s1);
+
+    std::cout << "Bitte zweiten Text eingeben (ggfs. mit Leerzeichen): ? ";
+    std::string s2;
+    std::getline(std::cin, s2);
+
+    std::cout << "Ergebnis my_strconcat(): " << my_strconcat(s1.data(), s2.data()) << std::endl;
+
+    return 0;
+}
+
+std::size_t cstrlen(const char* str) {
+    std::size_t s;
+    for (s = 0; str[s] != 0; ++s)
+        ;
+    return s;
+}
+
+char* my_strconcat(const char* ptr1, const char* ptr2) {
+    char*       res = new char[cstrlen(ptr1) + cstrlen(ptr2) + 1];
+    std::size_t i;
+    for (i = 0; ptr1[i] != 0; ++i) {
+        res[i] = ptr1[i];
+    }
+
+    for (std::size_t k = 0; ptr2[k] != 0; ++k) {
+        res[i + k] = ptr2[k];
+    }
+
+    res[cstrlen(ptr1) + cstrlen(ptr2)] = 0;
+    return res;
+}
+
